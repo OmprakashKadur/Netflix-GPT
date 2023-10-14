c027b83c1b94e40d76e7afcd61c18a7368a76c5a
Kadur Omee <123233570+OmprakashKadur@users.noreply.github.com>
2023-10-14 13:37:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-green-500 underline">
+     Namaste React- Neflix
+    </h1>
+  );
+}
+
+export default App;
